72b484eb6046d45fbc255c01f327aa14b819d8cd
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-08-16 22:55:16 +0200

    Update index.js
    updates the text of HTML elements dynamically

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function displayMenuItems(menu) {
             // Create a list item element
             const listItem = document.createElement('li');
             // Set the text content of the list item element to the item name
-
+            listItem.textContent = item;
             // Attach a click event listener to the list item to add it to the order
 
             // Append the list item to the list of items
