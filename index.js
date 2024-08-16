8267e87d6eae7e6c6e1fd51ac33bc52f49d5aa7b
modiribrodie <163874746+modiribrodie@users.noreply.github.com>
2024-08-16 23:02:31 +0200

    Click event listener
    this will create an interactive shopping list

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function displayMenuItems(menu) {
             // Set the text content of the list item element to the item name
             listItem.textContent = item;
             // Attach a click event listener to the list item to add it to the order
-
+            listItem.addEventListener('click', () => addToOrder(item));
             // Append the list item to the list of items
 
             
